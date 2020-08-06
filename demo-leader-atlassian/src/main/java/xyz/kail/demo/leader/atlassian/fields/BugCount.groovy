dde56a82afa5c9f9cf99aa1b9420831014ca03fa
kaibin.yang <ykb553@163.com>
2020-08-06 16:40:55 +0800

    bugcount

---
+++ demo-leader-atlassian/src/main/java/xyz/kail/demo/leader/atlassian/fields/BugCount.groovy
@@ -0,0 +1,23 @@
+package xyz.kail.demo.leader.atlassian.fields
+
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.link.LinkCollection
+
+// region 代码提示
+Issue issue = ComponentAccessor.issueManager.getIssueObject("ARCH-1")
+// endregion
+
+def bugProjectKeys = []
+def bugCount = 0D
+
+LinkCollection linkCollection = ComponentAccessor.issueLinkManager.getLinkCollectionOverrideSecurity(issue)
+for (Issue linkIssue : linkCollection.allIssues) {
+    def linkProject = linkIssue.projectObject
+    if (!bugProjectKeys.isEmpty() && bugProjectKeys.contains(linkProject.key) || linkProject.name.contains("【QA】") ) {
+        bugCount++
+    }
+}
+return bugCount
+
