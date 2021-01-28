d50afa7185708e9d2de0e5acf9093013dc56afe5
kaibin.yang <ykb553@163.com>
2021-01-28 14:50:39 +0800

    fix

--- demo-atlassian-scriptrunner/src/main/java/xyz/kail/demo/leader/atlassian/conditions/NotMemberOfRole.groovy
+++ demo-atlassian-scriptrunner/src/main/java/xyz/kail/demo/leader/atlassian/conditions/NotMemberOfRole.groovy
@@ -7,4 +7,4 @@ import com.atlassian.jira.component.ComponentAccessor
  * 当前用户非 '角色名称' 时可以操作
  */
 User currentUser = ComponentAccessor.jiraAuthenticationContext.user.directoryUser
-return !ComponentAccessor.groupManager.isUserInGroup(currentUser.name, "天天拍车测试部")
+return !ComponentAccessor.groupManager.isUserInGroup(currentUser.name, "角色名称")
